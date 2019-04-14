cd3c11e2d3b29ad39c0335b37fa111d14dc77881
Pr00sty <carlsberg.pawel@wp.pl>
2019-04-14 18:38:46 +0200

    add class Weather, read list of Weather objects

---
+++ src/main/java/firstWebserviceClient/weatherApp/Weather.java
@@ -0,0 +1,36 @@
+package firstWebserviceClient.weatherApp;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Weather {
+    private int id;
+    private String main;
+    private String description;
+    private String icon;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    @Override
+    public String toString() {
+        return "Weather{" +
+                "id=" + id +
+                ", main='" + main + '\'' +
+                ", description='" + description + '\'' +
+                ", icon='" + icon + '\'' +
+                '}';
+    }
+}
